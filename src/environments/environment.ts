aef1eabfe3876e856225f343ba1e9f8e4b1fc158
Joel Rahman <joel@flowmatters.com.au>
2019-02-25 14:44:42 +1100

    updated environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  google_maps_api_key: 'WENFO_GOOGLE_MAPS_API_KEY',
+  catalog:'assets/static/catalog.json',
+  palettes:'assets/palettes',
   production: false
 };
 
