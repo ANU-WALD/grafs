943121956f11f490c4ccf4f95ae2fe7f84588be7
Joel Rahman <joel@flowmatters.com.au>
2019-02-25 14:31:08 +1100

    Renamed app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
+import { environment } from '../environments/environment'
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MapWaldModule } from 'map-wald';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    NgbModule.forRoot(),
+    AgmCoreModule.forRoot({
+      apiKey: environment.google_maps_api_key
+    }),
+    MapWaldModule.forRoot({ paths: [] }),
     AppRoutingModule
   ],
   providers: [],
